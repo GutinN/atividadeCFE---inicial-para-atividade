aa12710cfb5a549a718b0e7f8f02c2c901e2699f
GutinN <gugas3freitas@gmail.com>
2024-06-29 00:57:11 -0300

    teste1

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,10 @@
 //efeito de esconder formulário de cadastro
+
+
+$(document).ready(function () {
+    $("#botao-cadastrar").click(function () {
+        $("#form-cadastro").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+});
